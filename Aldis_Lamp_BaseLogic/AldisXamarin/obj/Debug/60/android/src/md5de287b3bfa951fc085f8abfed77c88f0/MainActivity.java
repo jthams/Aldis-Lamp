090812ec78481c60e00bdf5501ba1f795542e958
Jonathan T Hamel <hamelj2991@gmail.com>
2019-03-14 15:12:02 -0700

    added flashlight permissions

---
+++ Aldis_Lamp_BaseLogic/AldisXamarin/obj/Debug/60/android/src/md5de287b3bfa951fc085f8abfed77c88f0/MainActivity.java
@@ -0,0 +1,47 @@
+package md5de287b3bfa951fc085f8abfed77c88f0;
+
+
+public class MainActivity
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("AldisXamarin.MainActivity, AldisXamarin", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("AldisXamarin.MainActivity, AldisXamarin", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ Aldis_Lamp_BaseLogic/LED Simulation/Bulb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LED_Simulation
+{
+    public partial class Bulb : UserControl
+    {
+        public Bulb()
+        {
+            InitializeComponent();
+        }
+    }
+}
